a57cb0769c9553dfde46ab5c37dde1248b3c0d5a
Exateym <exateymus@gmail.com>
2024-10-24 02:19:02 +0500

    Создана библиотека MyArrays и сделана программа для работы с одномерным массивом

---
+++ Лабораторная работа №5/MyArrays/MyArrays.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyArrays
+{
+    public class MyOneDimensionalArray<T>
+    {
+        /// <summary> Внутреннее хранилище данных - одномерный массив. </summary>
+        private T[] data;
+
+        /// <summary> Конструктор может использовать как массив, так и несколько отдельных значений, передаваемых в качестве параметров. </summary>
+        public MyOneDimensionalArray(params T[] items) { data = items; }
+
+        /// <summary> Конструктор копирования. </summary>
+        public MyOneDimensionalArray(MyOneDimensionalArray<T> otherObject) { data = otherObject.data; }
+
+        /// <summary> Используется для получения копии хранилища данных. </summary>
+        /// <returns> Одномерный массив. </returns>
+        public T[] GetData() { return data; }
+
+        /// <summary> Используется для получения количества элементов массива. </summary>
+        public int GetLength() { return data.Length; }
+
+        /// <summary> Делает массив пустым. </summary>
+        public void MakeEmpty() { data = new T[0]; }
+
+        /// <summary> Для локализации сообщений исключительных ситуаций. </summary>
+        private static ResourceManager resourceManager = new ResourceManager("MyArrays.DebugMessages", typeof(MyOneDimensionalArray<T>).Assembly);
+
+        /// <summary> Метод-индексатор. Доступен get и set для элементов внутреннего хранилища данных. </summary>
+        public T this[int elementIndex]
+        {
+            get
+            {
+                if (data.Length == 0)
+                {
+                    throw new Exception(resourceManager.GetString("emptyArray", CultureInfo.CurrentCulture));
+                }
+                if (elementIndex < 0)
+                {
+                    throw new Exception(resourceManager.GetString("negativeIndexing", CultureInfo.CurrentCulture));
+                }
+                if (elementIndex >= data.Length)
+                {
+                    throw new Exception(resourceManager.GetString("outOfRange", CultureInfo.CurrentCulture));
+                }
+                return data[elementIndex];
+            }
+
+            set
+            {
+                if (data.Length == 0)
+                {
+                    throw new Exception(resourceManager.GetString("emptyArray", CultureInfo.CurrentCulture));
+                }
+                if (elementIndex < 0)
+                {
+                    throw new Exception(resourceManager.GetString("negativeIndexing", CultureInfo.CurrentCulture));
+                }
+                if (elementIndex >= data.Length)
+                {
+                    throw new Exception(resourceManager.GetString("outOfRange", CultureInfo.CurrentCulture));
+                }
+                data[elementIndex] = value;
+            }
+        }
+
+        /// <summary> Удаляет элемент, уменьшая длину массива. </summary>
+        /// <remarks> Все элементы, идущие после удалённого смещаются влево. </remarks>
+        /// <param name="elementIndex"> Индекс элемента, который должен быть удалён. </param>
+        public void RemoveElement(int elementIndex)
+        {
+            if (data.Length == 0)
+            {
+                throw new Exception(resourceManager.GetString("negativeLength", CultureInfo.CurrentCulture));
+            }
+            if (elementIndex < 0)
+            {
+                throw new Exception(resourceManager.GetString("negativeIndexing", CultureInfo.CurrentCulture));
+            }
+            if (elementIndex >= data.Length)
+            {
+                throw new Exception(resourceManager.GetString("outOfRange", CultureInfo.CurrentCulture));
+            }
+
+            T[] newData = new T[data.Length - 1];
+            int index = 0;
+
+            // Копируем всё содержимое исходного массива в новый массив, исключая элемент под указанным индексом:
+
+            // Увеличиваем index до тех пор, пока значение не будет соответствовать elementIndex;
+            while (index < elementIndex)
+            {
+                newData[index] = data[index];
+                index++;
+            }
+
+            // Пропускаем элемент под указанным индексом;
+            index++;
+
+            // Увеличиваем index, чтобы скопировать оставшиеся элементы исходного массива.
+            while (index < data.Length)
+            {
+                newData[index - 1] = data[index];
+                index++;
+            }
+
+            data = newData;
+        }
+
+        /// <summary> Вставляет элемент, увеличивая длину массива. </summary>
+        /// <remarks> Новый элемент встаёт на позицию уже существующего, сдвигая его и все последующие элементы вправо. </remarks>
+        /// <param name="elementIndex"> Индекс, на который должен встать новый элемент. </param>
+        /// <param name="value"> Значение нового элемента. </param>
+        public void InsertElement(int elementIndex, T value)
+        {
+            if (data.Length == int.MaxValue)
+            {
+                throw new Exception(resourceManager.GetString("tooLong", CultureInfo.CurrentCulture));
+            }
+            if (elementIndex < 0)
+            {
+                throw new Exception(resourceManager.GetString("negativeIndexing", CultureInfo.CurrentCulture));
+            }
+            if (elementIndex >= data.Length)
+            {
+                throw new Exception(resourceManager.GetString("outOfRange", CultureInfo.CurrentCulture));
+            }
+
+            T[] newData = new T[data.Length + 1];
+            int index = 0;
+
+            // Копируем всё содержимое исходного массива данных в новый массив, учитывая новый элемент для подстановки под указанный индекс:
+
+            // Увеличиваем index до тех пор, пока значение не будет соответствовать elementIndex;
+            while (index < elementIndex)
+            {
+                newData[index] = data[index];
+                index++;
+            }
+
+            // В элементе нового массива под указанным индексом будет записано переданное значение;
+            newData[index] = value;
+
+            // Увеличиваем index, чтобы перебрать оставшиеся элементы исходного массива.
+            while (index < data.Length)
+            {
+                newData[index + 1] = data[index];
+                index++;
+            }
+
+            data = newData;
+        }
+
+        /// <summary> Добавляет элемент, увеличивая длину массива. </summary>
+        /// <remarks> Новый элемент будет являться новым концом массива. </remarks>
+        /// <param name="value"> Значение нового элемента. </param>
+        public void AddElementToEnd(T value)
+        {
+            if (data.Length == int.MaxValue)
+            {
+                throw new Exception(resourceManager.GetString("tooLong", CultureInfo.CurrentCulture));
+            }
+
+            T[] newData = new T[data.Length + 1];
+            int index = 0;
+            while (index < data.Length)
+            {
+                newData[index] = data[index];
+                index++;
+            }
+            newData[index] = value;
+            data = newData;
+        }
+    }
+}
---
+++ Лабораторная работа №5/Programs/OneDimentionalArray/Program.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Resources;
+using MyArrays;
+using System.Security.Cryptography;
+
+namespace OneDimentionalArray
+{
+    internal class Program
+    {
+        /// <summary> Настраивает окно консоли программы. </summary>
+        static void ConfigureProgramConsole()
+        {
+            Console.Title = "Работа с одномерным массивом int";
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.Clear();
+        }
+
+        /// <summary> Даёт время пользователю прочитать отклик программы. </summary>
+        static void SetPause()
+        {
+            Console.Write("\nПоставлена пауза. Нажмите любую клавишу, чтобы продолжить.");
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        static MyOneDimensionalArray<int> RandomizeData()
+        {
+            Random randomizer = new Random();
+            int arrayLength = randomizer.Next(3, 17);
+            int[] data = new int[arrayLength];
+            for (int index = 0; index < arrayLength; index++)
+            {
+                int newNumber = randomizer.Next(10);
+                if (newNumber != 0)
+                {
+                    bool isNegativeNumber = false;
+                    if (randomizer.Next(2) == 0)
+                    {
+                        isNegativeNumber = true;
+                    }
+                    if (isNegativeNumber)
+                    {
+                        newNumber = -newNumber;
+                    }
+                }
+                data[index] = newNumber;
+            }
+            return new MyOneDimensionalArray<int>(data);
+        }
+
+        /// <summary> Используется как служебная часть для методов подбора спряжения существительного к соответствующему числительному. </summary>
+        static int SimplificationNumeral(int number)
+        {
+            if (number > 19 && number < 100)
+                return number % 10;
+            if (number > 99)
+            {
+                number = number % 100;
+                if (number > 19 && number < 100)
+                    return number % 10;
+            }
+            return number;
+        }
+
+
+        /// <summary> Спрягает слово "элемент". В качестве параметра берётся числительное. </summary>
+        static string MatchingWordElementWithNumeral(int number)
+        {
+            number = SimplificationNumeral(number);
+            string foundation = "элемент";
+            if (number == 0 || number > 4)
+            {
+                return foundation + "ов";
+            }
+            if (number == 1)
+            {
+                return foundation;
+            }
+            return foundation + "а";
+        }
+
+        static void PrintArrayDataIntoConsole(in MyOneDimensionalArray<int> numbers)
+        {
+            int quantity = numbers.GetLength();
+            if (quantity == 0)
+            {
+                Console.WriteLine("Массив не содержит ни одного элемента.");
+                return;
+            }
+            Console.WriteLine($"Массив содержит {quantity} {MatchingWordElementWithNumeral(quantity)}:");
+            string result = "[";
+            int index = 0;
+            while (index < quantity)
+            {
+                result += numbers[index];
+                index++;
+                if (index != quantity)
+                {
+                    result += ", ";
+                }
+            }
+            result += "]";
+            Console.WriteLine(result);
+        }
+
+        static int ReadIntNumberFromUserInput()
+        {
+            Console.Write("Введите число int: ");
+            int number;
+            bool isSuitableValue = int.TryParse(Console.ReadLine(), out number);
+            if (!isSuitableValue) {
+                throw new Exception("Введённые данные не могут быть преобразованы в ожидаемый тип!");
+            }
+            return number;
+        }
+
+        /// <summary> Просматривает массив слева-направо в поиске отрицательного числа. </summary>
+        /// <returns> Если -1, то отрицательных чисел не найдено, иначе индекс такого числа. </returns>
+        static int FindIndexOfFirstNegativeNumber(in MyOneDimensionalArray<int> numbers)
+        {
+            for (int index = 0; index < numbers.GetLength(); index++)
+            {
+                if (numbers[index] < 0)
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+
+        static void PrintErrorIntoConsole(string errorContent)
+        {
+            ConsoleColor foregroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(errorContent);
+            Console.ForegroundColor = foregroundColor;
+        }
+        static void Main(string[] args)
+        {
+            ConfigureProgramConsole();
+            // Создаётся пустой массив, с которым можно в дальнейшем работать.
+            MyOneDimensionalArray<int> numbers = new MyOneDimensionalArray<int>();
+        UserInterface:
+            try
+            {
+                bool continueRunningProgram = true;
+                while (continueRunningProgram)
+                {
+                    Console.WriteLine("1. Завершить работу консольного приложения.");
+                    Console.WriteLine("2. Узнать информацию о массиве.");
+                    Console.WriteLine("3. Сгенерировать новый случайный массив.");
+                    Console.WriteLine("4. Добавить новый элемент в конец массива.");
+                    Console.WriteLine("5. Вставить новый элемент по индексу уже существующего.");
+                    Console.WriteLine("6. Удалить элемент по индексу.");
+                    Console.WriteLine("7. Сделать массив пустым.");
+                    Console.WriteLine("8. Удалить первый отрицательный элемент.");
+                    Console.Write("Введите кодовое слово опции работы с программой: ");
+                    string codeWord = Console.ReadLine();
+                    int index, number, quantity = numbers.GetLength();
+                    Console.WriteLine();
+                    switch (codeWord)
+                    {
+                        case "":
+                            PrintErrorIntoConsole("Ошибка пустого ввода!");
+                            break;
+                        case "1":
+                            continueRunningProgram = false;
+                            Console.WriteLine("Работа с программой завершена.");
+                            break;
+                        case "2":
+                            PrintArrayDataIntoConsole(numbers);
+                            break;
+                        case "3":
+                            numbers = RandomizeData();
+                            Console.WriteLine("Сгенерирован новый массив.");
+                            PrintArrayDataIntoConsole(numbers);
+                            break;
+                        case "4":
+                            Console.WriteLine("Массив на данный момент времени:");
+                            PrintArrayDataIntoConsole(numbers);
+                            Console.WriteLine("Запрос значения числа.");
+                            number = ReadIntNumberFromUserInput();
+                            numbers.AddElementToEnd(number);
+                            Console.WriteLine("Успешно добавлен новый элемент в конец массива.");
+                            Console.WriteLine("Массив на данный момент времени:");
+                            PrintArrayDataIntoConsole(numbers);
+                            break;
+                        case "5":
+                            if (quantity == 0)
+                            {
+                                Console.WriteLine("Массив не содержит ни одного элемента.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Массив на данный момент времени:");
+                                PrintArrayDataIntoConsole(numbers);
+                                Console.WriteLine("Запрос индекса уже существующего элемента массива.");
+                                index = ReadIntNumberFromUserInput();
+                                Console.WriteLine("Запрос значения числа.");
+                                number = ReadIntNumberFromUserInput();
+                                numbers.InsertElement(index, number);
+                                Console.WriteLine("Новый элемент успешно вставлен по индексу в массив.");
+                                Console.WriteLine("Массив на данный момент времени:");
+                                PrintArrayDataIntoConsole(numbers);
+                            }
+                            break;
+                        case "6":
+                            if (quantity == 0)
+                            {
+                                Console.WriteLine("Массив не содержит ни одного элемента.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Массив на данный момент времени:");
+                                PrintArrayDataIntoConsole(numbers);
+                                Console.WriteLine("Запрос индекса уже существующего элемента массива.");
+                                index = ReadIntNumberFromUserInput();
+                                numbers.RemoveElement(index);
+                                Console.WriteLine("Элемент под указанным индексом удалён из массива успешно.");
+                                Console.WriteLine("Массив на данный момент времени:");
+                                PrintArrayDataIntoConsole(numbers);
+                            }
+                            break;
+                        case "7":
+                            numbers.MakeEmpty();
+                            Console.WriteLine("Массив был успешно очищен до пустого.");
+                            break;
+                        case "8":
+                            if (quantity == 0)
+                            {
+                                Console.WriteLine("Массив не содержит ни одного элемента.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Массив на данный момент времени:");
+                                PrintArrayDataIntoConsole(numbers);
+                                index = FindIndexOfFirstNegativeNumber(numbers);
+                                if (index == -1)
+                                {
+                                    Console.WriteLine("Массив не содержит отрицательных чисел.");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Было найдено число {numbers[index]} под элементом массива с индексом {index}.");
+                                    numbers.RemoveElement(index);
+                                    Console.WriteLine("Отрицательное число было успешно удалено.");
+                                    Console.WriteLine("Массив на данный момент времени:");
+                                    PrintArrayDataIntoConsole(numbers);
+                                }
+                            }
+                            break;
+                        default:
+                            PrintErrorIntoConsole("В списке отсутствует вариант с указанным кодовым словом!");
+                            break;
+                    }
+                    SetPause();
+                }
+                return;
+            }
+            catch (Exception error)
+            {
+                PrintErrorIntoConsole(error.Message);
+                SetPause();
+                goto UserInterface;
+            }
+        }
+    }
+}
