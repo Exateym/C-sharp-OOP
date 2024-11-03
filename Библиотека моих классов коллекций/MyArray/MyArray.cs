682d14155522ab5e477b2e0778c7f50020308381
Exateym <exateymus@gmail.com>
2024-11-04 03:09:49 +0500

    Создано пространство имён MyGenericCollections с параметризованными классами MyArray и MySet

---
+++ Библиотека моих классов коллекций/MyArray/MyArray.cs
@@ -0,0 +1,258 @@
+﻿using System;
+
+namespace MyGenericCollections
+{
+    namespace MyArray
+    {
+        public class MyArray<T>
+        {
+            /// <summary> Параметризованный одномерный массив в качестве внутреннего сокрытого хранилища данных. </summary>
+            protected T[] storage;
+
+            /// <summary> Свойство для получения длины массива. </summary>
+            public int Length => storage.Length;
+
+            /// <summary> Делает массив пустым. </summary>
+            public void MakeEmpty() => storage = new T[0];
+
+            /// <summary> Конструктор без параметров инициализирует объект пустым массивом. </summary>
+            public MyArray() => MakeEmpty();
+
+            /// <summary> Конструктор копирования. </summary>
+            public MyArray(in MyArray<T> otherObject)
+            {
+                if (otherObject == null)
+                {
+                    throw new ArgumentNullException("value", "Нельзя инициализировать новый объект по пустой ссылке!");
+                }
+                storage = new T[otherObject.Length];
+                Array.Copy(otherObject.storage, storage, otherObject.Length);
+            }
+
+            /// <summary> Свойство для проверки на пустой массив. </summary>
+            public bool IsEmpty => Length == 0;
+
+            /// <summary> Метод-индексатор. Доступен get и set для элементов внутреннего хранилища данных. </summary>
+            public T this[int elementIndex]
+            {
+                get
+                {
+                    if (IsEmpty)
+                    {
+                        throw new InvalidOperationException("Массив не содержит ни одного элемента!");
+                    }
+                    if (elementIndex < 0)
+                    {
+                        throw new IndexOutOfRangeException("Индексация с помощью отрицательных чисел не доступна!");
+                    }
+                    if (elementIndex >= Length)
+                    {
+                        throw new ArgumentOutOfRangeException("index", "Элемента с указанным индексом не существует в массиве!");
+                    }
+                    return storage[elementIndex];
+                }
+
+                set
+                {
+                    if (IsEmpty)
+                    {
+                        throw new InvalidOperationException("Массив не содержит ни одного элемента!");
+                    }
+                    if (elementIndex < 0)
+                    {
+                        throw new IndexOutOfRangeException("Индексация с помощью отрицательных чисел не доступна!");
+                    }
+                    if (elementIndex >= Length)
+                    {
+                        throw new ArgumentOutOfRangeException("index", "Элемента с указанным индексом не существует в массиве!");
+                    }
+                    storage[elementIndex] = value;
+                }
+            }
+
+            /// <summary> Удаляет элемент, уменьшая длину массива. </summary>
+            /// <remarks> Все элементы, идущие после удалённого смещаются влево. </remarks>
+            /// <param name="elementIndex"> Индекс элемента, который должен быть удалён. </param>
+            public void RemoveElement(int elementIndex)
+            {
+                if (IsEmpty)
+                {
+                    throw new InvalidOperationException("Массив не содержит ни одного элемента!");
+                }
+                if (elementIndex < 0)
+                {
+                    throw new IndexOutOfRangeException("Индексация с помощью отрицательных чисел не доступна!");
+                }
+                if (elementIndex >= Length)
+                {
+                    throw new ArgumentOutOfRangeException("index", "Элемента с указанным индексом не существует в массиве!");
+                }
+
+                T[] newStorage = new T[Length - 1];
+                // Копирование элементов до указанного индекса.
+                Array.Copy(storage, 0, newStorage, 0, elementIndex);
+                // Копирование элементов после указанного индекса.
+                Array.Copy(storage, elementIndex + 1, newStorage, elementIndex, Length - elementIndex - 1);
+                storage = newStorage;
+            }
+
+            /// <summary> Вставляет элемент, увеличивая длину массива. </summary>
+            /// <remarks> Новый элемент встаёт на позицию уже существующего, сдвигая его и все последующие элементы вправо. </remarks>
+            /// <param name="elementIndex"> Индекс, на который должен встать новый элемент. </param>
+            /// <param name="value"> Значение нового элемента. </param>
+            public void InsertElement(int elementIndex, T value)
+            {
+                if (Length == int.MaxValue)
+                {
+                    throw new OverflowException("Включение нового элемента приведёт к слишком большой длине массива!");
+                }
+                if (elementIndex < 0)
+                {
+                    throw new IndexOutOfRangeException("Индексация с помощью отрицательных чисел не доступна!");
+                }
+                if (elementIndex >= Length)
+                {
+                    throw new ArgumentOutOfRangeException("index", "Элемента с указанным индексом не существует в массиве!");
+                }
+
+                T[] newStorage = new T[Length + 1];
+                // Копирование элементов до указанного индекса.
+                Array.Copy(storage, 0, newStorage, 0, elementIndex);
+                // В элементе нового массива под указанным индексом будет записано переданное значение.
+                newStorage[elementIndex] = value;
+                // Копирование элементов после указанного индекса.
+                Array.Copy(storage, elementIndex, newStorage, elementIndex + 1, Length - elementIndex);
+                storage = newStorage;
+            }
+
+            /// <summary> Добавляет элемент, увеличивая длину массива. </summary>
+            /// <remarks> Новый элемент будет являться новым концом массива. </remarks>
+            /// <param name="value"> Значение нового элемента. </param>
+            public void AddElementToEnd(T value)
+            {
+                if (Length == int.MaxValue)
+                {
+                    throw new OverflowException("Включение нового элемента приведёт к слишком большой длине массива!");
+                }
+
+                T[] newStorage = new T[Length + 1];
+                // Копирование всех элементов в новый массив.
+                Array.Copy(storage, newStorage, Length);
+                newStorage[Length] = value;
+                storage = newStorage;
+            }
+
+            /// <summary> Глубокая проверка на равенство коллекций. </summary>
+            /// <remarks>
+            /// Чтобы проверка могла корректно сравнить элементы массивов,
+            /// у них должен быть переопределён метод Equals для глубокой проверки на равенство.
+            /// </remarks>
+            /// <returns> Значение true, если указанный объект равен текущему объекту, в противном случае — значение false. </returns>
+            public override bool Equals(object otherObject)
+            {
+                // Оператор is проверяет, является ли объект экземпляром указанного типа.
+                /* Использование is вместе с условным присваиванием.
+                В этом случае, если otherObject действительно является экземпляром MyArray<T>,
+                то оператор is создает переменную otherMyArray, которая ссылается на тот же объект.
+                После создания ссылки с типом MyArray<T> можно обращаться к полям, характерным для этого класса. */
+                if (otherObject is MyArray<T> otherMyArray)
+                {
+                    if (Length != otherMyArray.Length)
+                    {
+                        return false;
+                    }
+                    for (int index = 0; index < Length; index++)
+                    {
+                        if (!storage[index].Equals(otherMyArray.storage[index]))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+
+            /// <summary> Служит для получения хэш-кода этой коллекции. </summary>
+            /// <remarks> Важно, чтобы у объектов коллекции был переопределён метод GetHashCode. </remarks>
+            /// <returns> Хэш-код для текущего объекта как значение int. </returns>
+            public override int GetHashCode()
+            {
+                int hash = 17;
+                foreach (var item in storage)
+                {
+                    // Оператор ?. позволяет безопасно вызывать методы или доступ к свойствам на объекте, который может быть null.
+                    // Оператор ?? используется для задания значения по умолчанию, если выражение слева равно null.
+                    hash = hash * 31 + (item?.GetHashCode() ?? 0);
+                }
+                return hash;
+            }
+
+            /// <summary> Общая проверка на равенство коллекций. </summary>
+            /// <remarks>
+            /// Сперва используется сравнение по ссылкам.
+            /// Если ссылки разные, они проверяются на null.
+            /// Если ссылки на объекты не null, происходит глубокое сравнивание объектов.
+            /// </remarks>
+            /// <returns> Значение true, когда оба объекта имеют одинаковые ссылки или равны по правилам метода Equals; иначе false. </returns>
+            public static bool operator ==(MyArray<T> left, MyArray<T> right)
+            {
+                if (ReferenceEquals(left, right)) return true;
+                if (left is null || right is null) return false;
+                return left.Equals(right);
+            }
+
+            /// <summary> Общая проверка на неравенство коллекций. </summary>
+            /// <remarks> Использует функционал оператора ==. </remarks>
+            public static bool operator !=(MyArray<T> left, MyArray<T> right)
+            {
+                return !(left == right);
+            }
+
+            /// <summary>
+            /// Применяет метод ToString к элементам массива,
+            /// чтобы получить строку с перечислением строковых представлений объектов,
+            /// обрамлённую квадратными скобками.
+            /// </summary>
+            /// <remarks> Работает корректно только если метод ToString переопределён у объектов коллекции. </remarks>
+            public override string ToString()
+            {
+                if (IsEmpty)
+                {
+                    return "[]";
+                }
+
+                string result = "[";
+                for (int index = 0; index < Length; index++)
+                {
+                    // Если элемент равен null, его представление отображается как "null".
+                    result += storage[index]?.ToString() ?? "null";
+                    if (index < Length - 1)
+                    {
+                        result += ", ";
+                    }
+                }
+                return result + "]";
+            }
+
+            /// <summary> Находит индекс указанного элемента в массиве. </summary>
+            /// <remarks>
+            /// Проходит по элементам коллекции слева-направо.
+            /// Работает корректно только, если у класса элементов коллекции переопределён метод Equals для глубокой проверки на равенство.
+            /// </remarks>
+            /// <param name="value"> Значение для поиска. </param>
+            /// <returns> Индекс элемента, если он найден; иначе -1. </returns>
+            public int GetIndexOfFirstMatch(T value)
+            {
+                for (int index = 0; index < Length; index++)
+                {
+                    if (value.Equals(storage[index]))
+                    {
+                        return index;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+}
---
+++ Библиотека моих классов коллекций/MySet/MySet.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using MyGenericCollections.MyArray;
+
+namespace MyGenericCollections
+{
+    namespace MySet
+    {
+        public class MySet<T> : MyArray<T>
+        {
+            // Скрытие или перезапись унаследованных методов, которые могут быть некорректно применены к множеству.
+            private new void AddElementToEnd(T value) { }
+            private new void InsertElement(int index, T value) { }
+
+            /// <summary> Метод-индексатор. Доступно только чтение элементов внутреннего хранилища с помощью get. </summary>
+            public new T this[int elementIndex]
+            {
+                get
+                {
+                    if (IsEmpty)
+                    {
+                        throw new Exception("Множество не содержит ни одного элемента!");
+                    }
+                    if (elementIndex < 0)
+                    {
+                        throw new Exception("Индексация с помощью отрицательных чисел не доступна!");
+                    }
+                    if (elementIndex >= Length)
+                    {
+                        throw new Exception("Элемента с указанным индексом не существует в множестве!");
+                    }
+                    return storage[elementIndex];
+                }
+            }
+
+
+
+            /// <summary> Добавляет элемент в множество, если он ещё не существует. </summary>
+            /// <remarks> Не генерирует исключение, когда добавляемый элемент уже существует в множестве. </remarks>
+            /// <param name="value"> Значение, которое нужно добавить. </param>
+            public void AddElement(T value)
+            {
+                if (GetIndexOfFirstMatch(value) == -1)
+                {
+                    base.AddElementToEnd(value);
+                }
+            }
+
+            /// <summary> Объединяет текущее множество с другим. </summary>
+            /// <param name="otherSet"> Множество, с которым нужно объединить. </param>
+            public void Union(in MySet<T> otherSet)
+            {
+                for (int index = 0; index < otherSet.Length; index++)
+                {
+                    AddElement(otherSet[index]);
+                }
+            }
+
+            /// <summary> Пересекает текущее множество с другим. </summary>
+            /// <param name="otherSet"> Множество, с которым нужно пересечь. </param>
+            public void Intersection(in MySet<T> otherSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+                for (int index = 0; index < Length; index++)
+                {
+                    // Проверка на наличие элемента в другом множестве.
+                    if (otherSet.GetIndexOfFirstMatch(this[index]) != -1)
+                    {
+                        // Добавление в результирующее множество.
+                        resultSet.AddElement(this[index]);
+                    }
+                }
+                // Очистка текущего множества и запись в него элементов из результирующего.
+                MakeEmpty();
+                for (int index = 0; index < resultSet.Length; index++)
+                {
+                    AddElement(resultSet[index]);
+                }
+            }
+
+            /// <summary> Удаляет из текущего множества все элементы, которые содержатся в другом множестве. </summary>
+            /// <param name="otherSet"> Множество, элементы которого нужно удалить из текущего множества. </param>
+            public void Difference(in MySet<T> otherSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+
+                // Происходит добавление элементов, которые есть в текущем множестве, но нет в другом.
+                for (int index = 0; index < Length; index++)
+                {
+                    if (otherSet.GetIndexOfFirstMatch(this[index]) == -1)
+                    {
+                        resultSet.AddElement(this[index]);
+                    }
+                }
+
+                // Очистка текущего множества и запись в него элементов из результирующего множества.
+                MakeEmpty();
+                for (int index = 0; index < resultSet.Length; index++)
+                {
+                    AddElement(resultSet[index]);
+                }
+            }
+
+            /// <summary> Выполняет симметрическую разность текущего множества с другим. </summary>
+            /// <param name="otherSet"> Множество, с которым нужно выполнить симметрическую разность. </param>
+            public void SymmetricDifference(in MySet<T> otherSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+
+                // Происходит добавление элементов, которые есть в текущем множестве, но нет в другом.
+                for (int index = 0; index < Length; index++)
+                {
+                    if (otherSet.GetIndexOfFirstMatch(this[index]) == -1)
+                    {
+                        resultSet.AddElement(this[index]);
+                    }
+                }
+
+                // Происходит добавление элементов, которые есть в другом множестве, но нет в текущем.
+                for (int index = 0; index < otherSet.Length; index++)
+                {
+                    if (GetIndexOfFirstMatch(otherSet[index]) == -1)
+                    {
+                        resultSet.AddElement(otherSet[index]);
+                    }
+                }
+
+                // Очистка текущего множества и запись в него элементов из результирующего множества.
+                MakeEmpty();
+                for (int index = 0; index < resultSet.Length; index++)
+                {
+                    AddElement(resultSet[index]);
+                }
+            }
+
+            public override bool Equals(object otherObject)
+            {
+                if (otherObject is MySet<T> otherSet)
+                {
+                    // Если длина различается, множества точно не равны.
+                    if (Length != otherSet.Length)
+                    {
+                        return false;
+                    }
+
+                    // Проверка, что каждый элемент текущего множества есть в другом множестве.
+                    for (int index = 0; index < Length; index++)
+                    {
+                        if (otherSet.GetIndexOfFirstMatch(this[index]) == -1)
+                        {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+                return false;
+            }
+
+            public override int GetHashCode()
+            {
+                int hash = 0;
+                for (int index = 0; index < Length; index++)
+                {
+                    /* Здесь используется XOR для накопления хэш-кодов каждого элемента.
+                    Это поможет получить одинаковый хэш-код для множеств с одинаковыми элементами,
+                    даже если порядок различается */
+                    hash ^= this[index]?.GetHashCode() ?? 0;
+                }
+                return hash;
+            }
+
+            /// <summary> Общая проверка на равенство множеств. </summary>
+            /// <remarks>
+            /// Если оба множества — null, они равны.
+            /// Если только одно из множеств — null, они не равны.
+            /// Если ни одна из ссылок на множества не является null, используется метод Equals для сравнения.
+            /// </remarks>
+            /// <returns> Значение true, когда оба множества — null или равны по правилам метода Equals; иначе false. </returns>
+            public static bool operator ==(MySet<T> left, MySet<T> right)
+            {
+                // Если оба множества — null, они равны.
+                if (ReferenceEquals(left, null) && ReferenceEquals(right, null))
+                {
+                    return true;
+                }
+
+                // Если только одно из множеств — null, они не равны.
+                if (ReferenceEquals(left, null) || ReferenceEquals(right, null))
+                {
+                    return false;
+                }
+
+                // Использование метода Equals для сравнения.
+                return left.Equals(right);
+            }
+
+            /// <summary> Общая проверка на неравенство множеств. </summary>
+            /// <remarks> Использует функционал оператора ==. </remarks>
+            public static bool operator !=(MySet<T> left, MySet<T> right)
+            {
+                return !(left == right);
+            }
+
+            /// <summary>
+            /// Применяет метод ToString к элементам множества,
+            /// чтобы получить строку с перечислением строковых представлений объектов,
+            /// обрамлённую фигурными скобками.
+            /// </summary>
+            /// <remarks> Работает корректно только если метод ToString переопределён у объектов множества. </remarks>
+            public override string ToString()
+            {
+                if (IsEmpty)
+                {
+                    return "{}";
+                }
+
+                string result = "{";
+                for (int index = 0; index < Length; index++)
+                {
+                    // Если элемент равен null, его представление отображается как "null".
+                    result += this[index]?.ToString() ?? "null";
+                    if (index < Length - 1)
+                    {
+                        result += ", ";
+                    }
+                }
+                return result + "}";
+            }
+        }
+
+        public static class MySetOperations
+        {
+            /// <summary>
+            /// Выполняет объединение двух множеств и возвращает новое множество,
+            /// содержащее уникальные элементы из обоих множеств.
+            /// </summary>
+            /// <returns> Новое множество, содержащее все уникальные элементы из первого и второго множеств. </returns>
+            public static MySet<T> GetUnion<T>(in MySet<T> firstSet, in MySet<T> secondSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+                resultSet.Union(firstSet);
+                resultSet.Union(secondSet);
+                return resultSet;
+            }
+
+            /// <summary>
+            /// Выполняет пересечение двух множеств и возвращает новое множество,
+            /// содержащее только элементы, присутствующие в обоих множествах.
+            /// </summary>
+            /// <returns> Новое множество, содержащее только общие элементы из первого и второго множеств. </returns>
+            public static MySet<T> GetIntersection<T>(in MySet<T> firstSet, in MySet<T> secondSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+                for (int index = 0; index < firstSet.Length; index++)
+                {
+                    if (secondSet.GetIndexOfFirstMatch(firstSet[index]) != -1)
+                    {
+                        resultSet.AddElement(firstSet[index]);
+                    }
+                }
+                return resultSet;
+            }
+
+            /// <summary>
+            /// Выполняет разность двух множеств и возвращает новое множество,
+            /// содержащее элементы, присутствующие только в первом множестве и отсутствующие во втором.
+            /// </summary>
+            /// <param name="firstSet"> Множество, из которого будут исключены элементы, присутствующие во втором множестве. </param>
+            /// <param name="secondSet"> Множество, элементы которого будут исключены из первого множества. </param>
+            /// <returns> Новое множество, содержащее элементы, присутствующие только в первом множестве. </returns>
+            public static MySet<T> GetDifference<T>(in MySet<T> firstSet, in MySet<T> secondSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+                for (int index = 0; index < firstSet.Length; index++)
+                {
+                    if (secondSet.GetIndexOfFirstMatch(firstSet[index]) == -1)
+                    {
+                        resultSet.AddElement(firstSet[index]);
+                    }
+                }
+                return resultSet;
+            }
+
+            /// <summary>
+            /// Выполняет симметрическую разность двух множеств и возвращает новое множество,
+            /// содержащее элементы, присутствующие в одном из множеств, но не в обоих одновременно.
+            /// </summary>
+            /// <returns> Новое множество, содержащее элементы, присутствующие только в одном из двух множеств. </returns>
+            public static MySet<T> GetSymmetricDifference<T>(in MySet<T> firstSet, in MySet<T> secondSet)
+            {
+                MySet<T> resultSet = new MySet<T>();
+                for (int index = 0; index < firstSet.Length; index++)
+                {
+                    if (secondSet.GetIndexOfFirstMatch(firstSet[index]) == -1)
+                    {
+                        resultSet.AddElement(firstSet[index]);
+                    }
+                }
+                for (int index = 0; index < secondSet.Length; index++)
+                {
+                    if (firstSet.GetIndexOfFirstMatch(firstSet[index]) == -1)
+                    {
+                        resultSet.AddElement(secondSet[index]);
+                    }
+                }
+                return resultSet;
+            }
+        }
+    }
+}
